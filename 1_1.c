86134309504d216f2536fc73570beb5cae2a719d
Raj-deepp <mrajdeep651@gmail.com>
2024-07-24 19:58:36 +0530

    Swap the contents of two variables enterd through the command line

--- 1_1.c
+++ 1_1.c
@@ -1,19 +1,29 @@
 #include<stdio.h>
 
 //Write a C program to swap the contents of two variables enterd through the command line using function and pointers
+
+#include <stdio.h>
+#include <stdlib.h>
+
 void swap(int *x, int *y){
     *x=*x+*y;
     *y=*x-*y;
     *x=*x-*y;
 }
 
-int main(){
-    int a, b;
-    printf("Enter two numbers: ");
-    scanf("%d %d",&a,&b);
-    printf("Before Swapping: %d %d\n",a,b);
+int main(int argc, char *argv[]) {
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
 
-    swap(&a,&b);
-    printf("After Swapping: %d %d",a,b);
+    printf("Before swapping:\n");
+    printf("num1 = %d\n", num1);
+    printf("num2 = %d\n", num2);
+
+    swap(&num1, &num2);
+
+    printf("After swapping:\n");
+    printf("num1 = %d\n", num1);
+    printf("num2 = %d\n", num2);
+
     return 0;
 }
